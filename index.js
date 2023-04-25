55a57434a2bcf511bcf0a614460a2085f7cdc559
Omid Kayhani <o.kayhani@gmail.com>
2023-04-25 17:14:39 +0300

    Done with the assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
